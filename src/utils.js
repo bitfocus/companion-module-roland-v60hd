bd838963f7031657aee15bb3cfd43dbd6a0c114e
Joseph Adams <josephdadams@gmail.com>
2024-01-22 07:59:58 -0500

    function name fix

--- src/utils.js
+++ src/utils.js
@@ -145,7 +145,7 @@ module.exports = {
 		let self = this;
 
 		if (!self.cmdPipe.includes('QPL:7')) { // No need to flood the buffer with these
-			self.sendCommmand('QPL:7')
+			self.sendCommand('QPL:7')
 		}
 	}
 }
